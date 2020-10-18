a57001db6edafe825c1e966bfc2f859a9a993368
adithya0076 <adithya0076@gmail.com>
2020-10-18 07:41:23 +0530

    new icon and added loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:Craftrips/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:async';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,7 +17,28 @@ class MyApp extends StatelessWidget {
         accentColor: Color(0xFFD8ECF1),
         scaffoldBackgroundColor: Color(0xFFF3F5F7),
       ),
-      home: HomeScreen(),
+      home: Splash2(),
+    );
+  }
+}
+
+class Splash2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 6,
+      navigateAfterSeconds: new HomeScreen(),
+      title: new Text(
+          ' ',
+          
+        
+        textScaleFactor: 2,
+      ),
+     image:  new Image.asset(
+          'assets/images/craf4.png'),
+      loadingText: Text("Loading"),
+      photoSize: 100.0,
+      loaderColor: Colors.blue,
     );
   }
 }
